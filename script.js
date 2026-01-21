abf36f63f834736f59a366c1f7e4f3b2ec0cfd4e
Rakib Uddin <mdrakib88022@gmail.com>
2026-01-21 16:57:58 +0600

    image & sound effect added

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
 let min = document.getElementById("min");
-    let mid = document.getElementById("mid");
-    let max = document.getElementById("max");
+let mid = document.getElementById("mid");
+let max = document.getElementById("max");
 
-    let speed = 160;
+// image change variables
+let isNear = false;
+let revertTimer = null;
 
-    min.addEventListener("click", function(){
-      speed = 300;
-      console.log(300)
-    } )
+// audio variables
+let sfx = new Audio("asset/monkey.wav");
+sfx.preload = "auto";
 
-    mid.addEventListener("click", function(){
-      speed = 140;
-      console.log(160)
-    } )
+// sound button logic
+let soundEnabled = false;
+let soundBtn = document.getElementById("soundBtn");
 
-    max.addEventListener("click", function(){
-      speed = 40;
-      console.log(50)
-    } )
+soundBtn.addEventListener("click", function () {
+  soundEnabled = !soundEnabled;
+
+  if (soundEnabled) {
+    soundBtn.textContent = "Sound ON ";
+  } else {
+    soundBtn.textContent = "Sound OFF";
+  }
+});
+
+// speed controller logic
+let speed = 160;
+
+min.addEventListener("click", function () {
+  speed = 300;
+  console.log(300);
+});
+
+mid.addEventListener("click", function () {
+  speed = 140;
+  console.log(140);
+});
+
+max.addEventListener("click", function () {
+  speed = 40;
+  console.log(40);
+});
 
 let balloon = document.getElementById("balloon");
-
 
 document.addEventListener("mousemove", function (e) {
   let mouseX = e.clientX;
@@ -34,26 +56,44 @@ document.addEventListener("mousemove", function (e) {
   let top = rect.top - padding;
   let bottom = rect.bottom + padding;
 
-  if(mouseX < right && mouseX > left && mouseY > top && mouseY < bottom) {
+  if (mouseX < right && mouseX > left && mouseY > top && mouseY < bottom) {
+
+    
+    if (revertTimer) {
+      clearTimeout(revertTimer);
+      revertTimer = null;
+    }
+
+    
+    if (!isNear) {
+      balloon.classList.add("near");
+      isNear = true;
+
+      if (soundEnabled) {
+        sfx.currentTime = 0;
+        sfx.play();
+      }
+    }
+
     let maxX = window.innerWidth - rect.width;
     let maxY = window.innerHeight - rect.height;
 
-    let ranX = Math.random() * (maxX + 1);
-    let ranY = Math.random() * (maxY + 1);
+    let newX = Math.floor(Math.random() * (maxX + 1));
+    let newY = Math.floor(Math.random() * (maxY + 1));
 
-    let newX = Math.floor(ranX);
-    let newY = Math.floor(ranY);
+    
+    balloon.style.transitionDuration = speed + "ms";
+    balloon.style.left = newX + "px";
+    balloon.style.top = newY + "px";
 
-    balloon.style.left = newX + "px"
-    balloon.style.top = newY + "px"
+  } else {
 
-
-
-    balloon.style.transitionDuration = speed + "ms"
-
-  }
-
-
-
-  
+      if (isNear && !revertTimer) {
+        revertTimer = setTimeout(function () {
+          balloon.classList.remove("near");
+          isNear = false;
+          revertTimer = null;
+        }, 300); // 0.3sec
+      }
+    }
 });
